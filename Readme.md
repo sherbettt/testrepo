073cde51477e7aabbcf1124a3059dcce25d49efc
kiko0217 <kirich_89@mail.ru>
2024-11-24 20:36:40 +0300

    change test2.sh and readme.md description

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
 ## Task from https://platform.productstar.ru/, Редакторы и работа с текстом
 *TASK [productstar.ru/work_with_editors](https://platform.productstar.ru/01d2bdbb/55387842-50c4-4081-8d89-025271094038?tab=tools)*
 
-**Description**: To feel *test.sh*. 
+**Description**: Создать *test2.sh* так, чтобы после математических операций с переменными a и b, мы получали число 3336. 
--- test2.sh
+++ test2.sh
@@ -1,8 +1,21 @@
 #!/bin/bash 
 
-declare -i a #объявление переменно a типа integer
-declare -i b; declare -i c
-a=12; b=8; c=$a+$b
-echo $c
-c=good
-echo $c
+declare -i a #объявление переменной a типа integer
+read -p "введи переменную равной 33: " a
+#a=33
+let a++ && echo "один раз инкрементируем переменную а: $a"
+b=DZ34
+let "b+=1" && echo "один раз инкрементируем переменную b: $b" # перменная b была не integer, поэтому и резуьтат равен 1
+b=DZ34
+a=${b/DZ/33}
+echo $a
+let "a+=1"; let "a+=1" && echo $a
+
+
+
+
+
+#let "var1 = 6" "var2 = 2" "var3=var1+var2"; echo $var3
+#let A=5+4; echo $A 
+#let A++; echo $A # инкремент
+#let "A = $1 + 30"; echo $A
